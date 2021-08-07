f589684c8bc3626e8b1f64941f9f15fe5fbf1d7f
Tanaes <jonsan@gmail.com>
2021-08-06 22:13:13 -0400

    added fofn script

---
+++ prep_bactopia_fofn.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import argparse
+import pandas as pd
+from glob import glob
+from os.path import join, basename, abspath
+from re import compile, match
+
+def import_sample_sheet(fp):
+    sample_sheet = pd.read_csv(fp,
+                               header=None,
+                               names=['Sample', 'i7','i5'])
+
+    return(sample_sheet)
+
+def parse_filename(fname, pattern):
+    m = match(pattern, fname)
+
+    if m:
+        seq_info = {'flowcell': m.groups()[0],
+                    'sample': m.groups()[1],
+                    'i7': m.groups()[2],
+                    'i5': m.groups()[3],
+                    'read': m.groups()[4]}
+        return(seq_info)
+    else:
+        return(False)
+
+def parse_seq_files(seq_fps,
+                    pattern):
+    seq_dict = {}
+    unmatched = []
+    for file in seq_fps:
+        seq_info = parse_filename(basename(file), pattern)
+
+        if seq_info:
+            seq_dict[abspath(file)] = seq_info
+        else:
+            unmatched.append(file)
+    return(seq_dict, unmatched)
+
+def match_pe_seqs(sample_df, seq_df):
+    seqs = {}
+    unmatched = []
+    for i, row in sample_df.iterrows():
+        sample = row['Sample']
+        sample_reads = seq_df.loc[seq_df['sample'] == sample,:]
+        if len(sample_reads) == 2:
+            seqs[sample] = {'r1': sample_reads.loc[sample_reads['read'] == 'R1'].index[0],
+                            'r2': sample_reads.loc[sample_reads['read'] == 'R2'].index[0]}
+        else:
+            unmatched.append(sample)
+    return(seqs)
+
+def format_fofn(pe_seqs,
+                runtype='paired-end',
+                extra=None):
+    
+
+    fofn_df = pd.DataFrame.from_dict(pe_seqs,
+                                     orient='index')
+
+    fofn_df.index.name = 'sample'
+
+    if extra:
+        fofn_df['extra'] = extra
+    else:
+        fofn_df['extra'] = ''
+
+    fofn_df['runtype'] = runtype
+
+    return(fofn_df)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--sample_sheet', 
+                    '-s',
+                    required=True,
+                    type=str)
+parser.add_argument('--output_fp', 
+                    '-o',
+                    required=True,
+                    type=str)
+parser.add_argument('--ilm_regex',
+                    '-r',
+                    type=str,
+                    default='^\d+_\d+_\d+_(\w+)_(.+?)_([AGCT]+)_([AGCT]+)_([R12]+)(.+)$')
+parser.add_argument('seq_dir',
+                    type=str)
+
+
+def main():
+    args = parser.parse_args()
+
+    sample_sheet_fp = args.sample_sheet
+    files_dir = args.seq_dir
+    ilm_regex = args.ilm_regex
+    fofn_fp = args.output_fp
+
+    sample_df = import_sample_sheet(sample_sheet_fp)
+
+    files_list = glob(join(files_dir,'*'))
+
+    ilm_re = compile(ilm_regex)
+    seq_dict, unmatched = parse_seq_files(files_list, ilm_re)
+
+    fofn_df = format_fofn(seq_dict)
+
+    fofn_df.to_csv(fofn_fp, sep='\t')
+
+
+if __name__ == "__main__":
+    main()
+
